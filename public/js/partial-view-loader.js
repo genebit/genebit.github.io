43ae220954f107453dcd15f53c8ddc7f61213684
genebit <genebitara@gmail.com>
2022-04-24 13:39:18 +0800

    Separated html for sidebar

--- public/js/partial-view-loader.js
+++ public/js/partial-view-loader.js
@@ -1,10 +1,11 @@
 // NOTES: To load a partial view, on HTML use <section insert-{partialViewName}></section>
 // Note that the partialViewName should not include the _ at start.
 
-$(document).ready(function () {
+$(document).ready(() => {
 	const path = {
 		// Add Partial views here
-		sidebar: "/public/shared/_sidebar.html",
+		sidebar: "/views/shared/_sidebar.html",
+		sidebarprofile: "/views/shared/_sidebar-profile.html",
 	}
 	$.each(path, function (key, value) {
 		loadPartialView(value, `insert-${key}`)
@@ -14,9 +15,7 @@ function loadPartialView(path, dataAttr) {
 	fetch(path)
 		.then((response) => response.text())
 		.then((html) => {
-			let parser = new DOMParser()
-			let doc = parser.parseFromString(html, "text/html")
-			$(`section[${dataAttr}]`).append(doc.documentElement)
+			$(`[${dataAttr}]`).append(html)
 		})
 		.catch((error) => {
 			console.warn("Failed to load partial view", error)
