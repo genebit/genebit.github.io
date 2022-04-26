edf62a12b7c5b718ccfdd3547070adcb91c78972
genebit <genebitara@gmail.com>
2022-04-26 22:00:00 +0800

    Changed path for partial views

--- public/js/partial-view-loader.js
+++ public/js/partial-view-loader.js
@@ -4,8 +4,8 @@
 $(document).ready(() => {
 	const path = {
 		// Add Partial views here
-		sidebar: "/_sidebar.html",
-		sidebarprofile: "/_sidebar-profile.html",
+		sidebar: "/views/sidebar.html",
+		sidebarprofile: "/views/sidebar-profile.html",
 	}
 	$.each(path, (key, value) => {
 		loadPartialView(value, `insert-${key}`)
